d105731cca4f72ef6a60e987421f1fa31b62c468
mboji <mbojic12@gmail.com>
2022-05-28 23:36:11 +0200

    Nodes and module

--- node/node.go
+++ node/node.go
@@ -1,8 +1,71 @@
 package node
 
+import (
+	"distributed/structures"
+	"fmt"
+)
+
 type INode interface {
 	GetAdders() string
 	GetPort() int
 	GetFullAddress() string
 	GetType() string
+	GetId() int
+}
+
+type Bootstrap struct {
+	IpAddress string `json:"ipAddress"`
+	Port      int    `json:"port"`
+}
+
+func (b *Bootstrap) GetAdders() string {
+	return b.IpAddress
+}
+
+func (b *Bootstrap) GetPort() int {
+	return b.Port
+}
+
+func (b *Bootstrap) GetFullAddress() string {
+	return fmt.Sprintf("%s:%d", b.IpAddress, b.Port)
+}
+
+func (b *Bootstrap) GetType() string {
+	return "Bootstrap"
+}
+
+func (b *Bootstrap) GetId() int {
+	return -1
+}
+
+type Worker struct {
+	Id          int                `json:"nodeId"`
+	IpAddress   string             `json:"ipAddress"`
+	Port        int                `json:"port"`
+	prev        int                `json:"-"`
+	next        int                `json:"-"`
+	JobId       int                `json:"-"`
+	FractalId   int                `json:"-"`
+	Connections []int              `json:"-"`
+	History     []structures.Point `json:"-"`
+}
+
+func (w *Worker) GetAdders() string {
+	return w.IpAddress
+}
+
+func (w *Worker) GetPort() int {
+	return w.Port
+}
+
+func (w *Worker) GetFullAddress() string {
+	return fmt.Sprintf("%s:%d", w.IpAddress, w.Port)
+}
+
+func (w *Worker) GetType() string {
+	return "Worker"
+}
+
+func (w *Worker) GetId() int {
+	return w.Id
 }
---
+++ structures/structures.go
@@ -0,0 +1,45 @@
+package structures
+
+type Stack []int
+
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *Stack) Push(val int) {
+	*s = append(*s, val)
+}
+
+func (s *Stack) Pop() (int, bool) {
+	if s.IsEmpty() {
+		return 0, false
+	} else {
+		index := len(*s) - 1   // Get the index of the top most element.
+		element := (*s)[index] // Index into the slice and obtain the element.
+		*s = (*s)[:index]      // Remove it from the stack by slicing it off.
+		return element, true
+	}
+}
+
+type Queue []int
+
+func (q *Queue) IsEmpty() bool {
+	return len(*q) == 0
+}
+
+func (q *Queue) Enqueue(val int) {
+	*q = append(*q, val)
+}
+
+func (q *Queue) Dequeue() (int, bool) {
+	if q.IsEmpty() {
+		return 0, false
+	}
+	elem := (*q)[0]
+	*q = (*q)[1:]
+	return elem, true
+}
+
+type Point struct {
+	x, y int
+}
